d93d0376e231475c0c421a957ad9a814c4c5add6
rjzhb <rjzhb222@163.com>
2023-08-14 22:16:11 +0800

    update

--- test/MultipleTest.cpp
+++ test/MultipleTest.cpp
@@ -2,13 +2,13 @@
 // Created by 86183 on 2023/3/22.
 //
 #include "gtest/gtest.h"
-#include "../src/MultipleManager/MultipleManager.h"
 
-
-TEST(MultipleManager, testMultiple) {
-    MultipleManager manager;
-    EXPECT_EQ(3, manager.multiple(1, 3));
-}
+//
+//
+//TEST(MultipleManager, testMultiple) {
+//    MultipleManager manager;
+//    EXPECT_EQ(3, manager.multiple(1, 3));
+//}
 
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
